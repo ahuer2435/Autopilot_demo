1015e22117f86dd2fb7843af3efc67a342b5aa19
ahuer2435 <18221079843@139.com>
2018-04-13 14:00:07 +0800

    On branch master  Your branch is up-to-date with 'origin/master'.
     Changes to be committed: add usbcan send and receive function.
    	modified:   catkin_ws/src/driver/usbcan_driver/CMakeLists.txt
    	deleted:    catkin_ws/src/driver/usbcan_driver/src/usbcan_driver_node.c
    	renamed:    catkin_ws/src/driver/usbcan_driver/src/usbcan_receive_node.cpp -> catkin_ws/src/driver/usbcan_driver/src/usbcan_node.cpp
    	deleted:    catkin_ws/src/driver/usbcan_driver/src/usbcan_send_node.cpp
    	modified:   catkin_ws/src/msg_convert/package.xml

--- catkin_ws/src/driver/usbcan_driver/src/usbcan_node.cpp
+++ catkin_ws/src/driver/usbcan_driver/src/usbcan_node.cpp
@@ -5,6 +5,14 @@
 #include "boost/thread.hpp"
 #include "msg_convert/vehicle_status.h"
 
+static unsigned gDevType = 0;
+static unsigned gDevIdx = 0;
+static unsigned gChMask = 0;
+static unsigned gBaud = 0;
+static unsigned gTxType = 0;
+static unsigned gTxSleep = 0;
+static unsigned gTxFrames = 0;
+
 #define RX_BUFF_SIZE  1000
 
 typedef struct {
@@ -14,17 +22,8 @@ typedef struct {
     unsigned error; // error(s) detected
 } RX_CTX;
 
-
-static unsigned gDevType = 0;
-static unsigned gDevIdx = 0;
-static unsigned gChMask = 0;
-static unsigned gBaud = 0;
-static unsigned gTxType = 0;
-static unsigned gTxSleep = 0;
-static unsigned gTxFrames = 0;
-
+ros::Publisher  pub_can_message;
 ros::Publisher pub_vechile_stata;
-
 
 int verify_frame(VCI_CAN_OBJ *can)
 {
@@ -43,8 +42,36 @@ int verify_frame(VCI_CAN_OBJ *can)
     return 0; // ext-frame ok
 }
 
+static void vehicle_cmd_callback(const msg_convert::vehicle_cmd& vehicle_cmd_input)
+{
+
+    static unsigned int index = 0;
+    VCI_CAN_OBJ can_message;
+    can_message.ID = index++;
+    can_message.TimeFlag = 0;
+    can_message.SendType = gTxType;
+    can_message.RemoteFlag = 0;
+    can_message.ExternFlag = 0;
+    can_message.DataLen = 5;
+    can_message.Data[0] = vehicle_cmd_input.steering_enable;
+    memcpy(&can_message.Data[1],&vehicle_cmd_input.steering_wheel_angle_cmd,4);
+    send_usbcan(&can_message);
+
+    can_message.ID = index++;
+    can_message.Data[0] = vehicle_cmd_input.brake_enable;
+    memcpy(&can_message.Data[1],&vehicle_cmd_input.brake_cmd,4);
+    send_usbcan(&can_message);
+
+    can_message.ID = index++;
+    can_message.Data[0] = vehicle_cmd_input.throttle_enable;
+    memcpy(&can_message.Data[1],&vehicle_cmd_input.throttle_cmd,4);
+    send_usbcan(&can_message);
+
+}
+
 int can_data_recevie_loop()
 {
+    msg_convert::vehicle_status vehicle_status;
     VCI_CAN_OBJ can[RX_BUFF_SIZE];
     memset(can, 0, sizeof(VCI_CAN_OBJ)*RX_BUFF_SIZE);
     int i,frame_size;
@@ -62,7 +89,10 @@ int can_data_recevie_loop()
                 printf("verify_frame fail.\n");
                 continue;
             }
-
+            vehicle_status.id = can[i].ID;
+            vehicle_status.datalen = can[i].DataLen;
+            memcpy(&vehicle_status.data,&can[i].Data,can[i].DataLen);
+            pub_vechile_stata.publish(vehicle_status);
         }
     }
     return 0;
@@ -76,19 +106,22 @@ void CanDataReceiveLoop(void)
 
 int main(int argc, char **argv)
 {
-    ros::init(argc, argv, "usbcan_recevie_node");
+    ros::init(argc, argv, "usbcan_send_node");
     ros::NodeHandle nh;
 
     gDevType = 16;
     gDevIdx = 0;
     gChMask = 1;
     gBaud = 0x1400;
-    gTxType = 0;
+    gTxType = 2;
     gTxSleep = 3;
     gTxFrames = 1000;
     init_usbcan( gDevType, gDevIdx, gChMask, gBaud, gTxType, gTxSleep, gTxFrames);
-    boost::thread reveive_loop(&CanDataReceiveLoop);
+
     pub_vechile_stata = nh.advertise<msg_convert::vehicle_status>("vehicle_stata", 2, true);
+
+    ros::Subscriber sub_vechile_cmd = nh.subscribe("vehicle_cmd", 10, vehicle_cmd_callback);
+    boost::thread reveive_loop(&CanDataReceiveLoop);
 
     ros::spin();
 
