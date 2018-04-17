d301ffb59520d8ba3e253aaafb581127b13835fb
ahuer2435 <18221079843@139.com>
2018-04-17 13:55:24 +0800

    On branch master  Your branch is up-to-date with 'origin/master'.
     Changes to be committed:convert ll to utm.
    	new file:   catkin_ws/src/localization/navsat_localization/CMakeLists.txt
    	new file:   catkin_ws/src/localization/navsat_localization/package.xml
    	new file:   catkin_ws/src/localization/navsat_localization/src/navsat_localization.cpp
    	new file:   catkin_ws/src/localization/navsat_localization/src/navsat_localization.h

---
+++ catkin_ws/src/localization/navsat_localization/src/navsat_localization.cpp
@@ -0,0 +1,39 @@
+#include <ros/ros.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <sensor_msgs/NavSatFix.h>
+
+#include "navsat_localization.h"
+
+ros::Publisher pub_pose;
+static void gps_callback(const sensor_msgs::NavSatFix& global_pose)
+{
+    geometry_msgs::PoseStamped pose;
+    double northing,easting;
+    std::string zone;
+    LLtoUTM(global_pose.latitude,global_pose.longitude,northing,easting,zone);
+
+    pose.header.frame_id = global_pose.header.frame_id;
+    pose.header.stamp = global_pose.header.stamp;
+    pose.pose.position.x = easting;
+    pose.pose.position.y = northing;
+    pose.pose.position.z = global_pose.altitude;
+
+    pub_pose.publish(pose);
+
+    return;
+}
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "navsat_localization_node");
+    ros::NodeHandle nh;
+
+    ros::Subscriber sub_gps = nh.subscribe("gps", 1, gps_callback);
+    pub_pose = nh.advertise<geometry_msgs::PoseStamped>("localization/pose",1,true);
+
+    ros::spin();
+
+    return 0;
+}
+
+//refernce: https://blog.csdn.net/CSDNhuaong/article/details/78510436
