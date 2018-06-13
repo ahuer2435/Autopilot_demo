6c4a7bf165b66cc37a22a51570f3cb5feb80c4a9
ahuer2435 <18221079843@139.com>
2018-06-13 16:35:19 +0800

    finish utm->world->gps.

--- catkin_ws/src/localization/gps_localization/src/gps_localization.cpp
+++ catkin_ws/src/localization/gps_localization/src/gps_localization.cpp
@@ -62,12 +62,10 @@ void createOrientation(utm_data last_utm, utm_data curr_utm)
 //gps坐标系与utm坐标系的关系.存储于transform_gps2utm_pose_
 void Gps2UtmTransform(const sensor_msgs::NavSatFixConstPtr& msg)
 {
-  double utm_x = 0;
-  double utm_y = 0;
   std::string  utm_zone_;
   LLtoUTM(msg->latitude, msg->longitude, utm_y_orign, utm_x_orign, utm_zone_);
 
-  transform_gps2utm_pose_.setOrigin(tf2::Vector3(utm_x_orign, utm_y_orign, msg->altitude));
+  transform_gps2utm_pose_.setOrigin(tf2::Vector3(utm_x_orign, utm_y_orign, 0.0));
   transform_gps2utm_pose_.setRotation(tf2::Quaternion::getIdentity());
   has_transform_utm_ = true;
   ROS_INFO_STREAM("has_transform_utm_ " << has_transform_utm_);
@@ -82,13 +80,13 @@ void Gps2WorldTransform(const sensor_msgs::NavSatFixConstPtr& msg)
   std::string  utm_zone_;
   LLtoUTM(msg->latitude, msg->longitude, utm_y, utm_x, utm_zone_);
 
-  transform_gps2world_pose_.setOrigin(tf2::Vector3(utm_x-utm_x_orign, utm_y-utm_y_orign, msg->altitude));
+  transform_gps2world_pose_.setOrigin(tf2::Vector3(utm_x-utm_x_orign, utm_y-utm_y_orign, 0.0));
   transform_gps2world_pose_.setRotation(tf2::Quaternion::getIdentity());
   has_transform_world_ = true;
   ROS_INFO_STREAM("has_transform_world_ " << has_transform_world_);
 }
 
-void computeTransform()
+void publishTF()
 {
     if (has_transform_utm_ && has_transform_world_)
     {
@@ -116,14 +114,33 @@ void computeTransform()
         transform_good_ = true;
 
         // 发布tf,utm与world_frame_id_之间的变换.
-        static tf2_ros::TransformBroadcaster utm_broadcaster_;
-        geometry_msgs::TransformStamped utm_transform_stamped;
-        utm_transform_stamped.header.stamp = ros::Time::now();
-        utm_transform_stamped.header.frame_id = world_frame_id_;
-        utm_transform_stamped.child_frame_id =  "utm";
-        utm_transform_stamped.transform = tf2::toMsg(utm_world_transform_);
-        utm_transform_stamped.transform.translation.z = 0.0;
-        utm_broadcaster_.sendTransform(utm_transform_stamped);
+        static tf2_ros::TransformBroadcaster utm_world_broadcaster_;
+        geometry_msgs::TransformStamped utm_world_transform_stamped;
+
+        static tf2_ros::TransformBroadcaster world_gps_broadcaster_;
+        geometry_msgs::TransformStamped world_gps_transform_stamped;
+
+        tf2::Transform transformed_world_gps;
+
+        //utm_world_transform_保存的是utm坐标系在world坐标系下的位置，latest_utm_pose_代表gps接收器在utm坐标系下的坐标，相乘可得gps接收器
+        //在world坐标系下坐标。
+        transformed_world_gps.mult(utm_world_transform_, latest_utm_pose_);
+        //因为latest_utm_pose_中只有位置，没有朝向，所以设置朝向
+        transformed_world_gps.setRotation(tf2::Quaternion::getIdentity());
+
+        utm_world_transform_stamped.header.stamp = ros::Time::now();
+        utm_world_transform_stamped.header.frame_id = "utm";
+        utm_world_transform_stamped.child_frame_id = world_frame_id_;
+        utm_world_transform_stamped.transform = tf2::toMsg(utm_world_trans_inverse_);
+        utm_world_transform_stamped.transform.translation.z = 0.0;
+        utm_world_broadcaster_.sendTransform(utm_world_transform_stamped);
+
+        world_gps_transform_stamped.header.stamp = utm_world_transform_stamped.header.stamp;
+        world_gps_transform_stamped.header.frame_id = world_frame_id_;
+        world_gps_transform_stamped.child_frame_id = gps_frame_id_;
+        world_gps_transform_stamped.transform = tf2::toMsg(transformed_world_gps);
+        world_gps_transform_stamped.transform.translation.z = 0.0;
+        world_gps_broadcaster_.sendTransform(world_gps_transform_stamped);
     }
 }
 
@@ -134,23 +151,25 @@ bool prepareGpsOdometry(nav_msgs::Odometry &gps_odom)
 
   if (transform_good_ && gps_updated_)
   {
-    tf2::Transform transformed_utm_gps;
+    tf2::Transform transformed_world_gps;
 
     //utm_world_transform_保存utm-->wrold坐标系变换，latest_utm_pose_代表gps接收器在utm坐标系下的坐标，相乘可得gps接收器
     //在world坐标系下坐标。
-    transformed_utm_gps.mult(utm_world_transform_, latest_utm_pose_);
+    transformed_world_gps.mult(utm_world_transform_, latest_utm_pose_);
     //因为latest_utm_pose_中只有位置，没有朝向，所以设置朝向
-    transformed_utm_gps.setRotation(tf2::Quaternion::getIdentity());
+    transformed_world_gps.setRotation(tf2::Quaternion::getIdentity());
+
 
     // Set header information stamp because we would like to know the robot's position at that timestamp
     //这里的world_frame_id_应该是odom，里程计坐标系
     gps_odom.header.frame_id = world_frame_id_;
     gps_odom.header.stamp = gps_update_time_;
+    gps_odom.child_frame_id = "gps";
 
     // Rotate the covariance as well
     //将旋转矩阵拓展为6x6矩阵，然后对协方差进行旋转。
     tf2::Matrix3x3 rot(utm_world_transform_.getRotation());
- #if 0
+
     Eigen::MatrixXd rot_6d(POSE_SIZE, POSE_SIZE);
     rot_6d.setIdentity();
 
@@ -166,7 +185,7 @@ bool prepareGpsOdometry(nav_msgs::Odometry &gps_odom)
 
     // Rotate the covariance
     latest_utm_covariance_ = rot_6d * latest_utm_covariance_.eval() * rot_6d.transpose();
-#endif
+
     // Now fill out the message. Set the orientation to the identity.
     //从transformed_utm_robot提取位姿信息
     tf2::toMsg(transformed_utm_gps, gps_odom.pose.pose);
@@ -174,7 +193,6 @@ bool prepareGpsOdometry(nav_msgs::Odometry &gps_odom)
 
     // Copy the measurement's covariance matrix so that we can rotate it later
     //跟新gps_odom中的协方差矩阵。
-#if 0
     for (size_t i = 0; i < POSE_SIZE; i++)
     {
       for (size_t j = 0; j < POSE_SIZE; j++)
@@ -182,7 +200,6 @@ bool prepareGpsOdometry(nav_msgs::Odometry &gps_odom)
         gps_odom.pose.covariance[POSE_SIZE * i + j] = latest_utm_covariance_(i, j);
       }
     }
-#endif
     // Mark this GPS as used
     gps_updated_ = false;
     new_data = true;
@@ -198,12 +215,6 @@ void publishPoseStamped()
     if(prepareGpsOdometry(gps_odom)){
         pub1_.publish(gps_odom);
     }
-}
-
-//发布world_frame_id_和gps_frame_id_的转化tf关系.
-void publishTF()
-{
-    computeTransform();
 }
 
 //将gps 位姿数据转化为utm位姿数据，gps位姿数据设置为utm坐标系的原点和朝向
@@ -233,32 +244,31 @@ void gpsFixCallback(const sensor_msgs::NavSatFixConstPtr& msg)
         std::string utm_zone_tmp;
         LLtoUTM(msg->latitude, msg->longitude, curr_utm.utm_y, curr_utm.utm_x, utm_zone_tmp);
 
-        latest_utm_pose_.setOrigin(tf2::Vector3(curr_utm.utm_x, curr_utm.utm_y, msg->altitude));
+        latest_utm_pose_.setOrigin(tf2::Vector3(curr_utm.utm_x, curr_utm.utm_y, 0.0));
         latest_utm_covariance_.setZero();
-#if 0   //这里运行出错,暂时没有找到原因.现行注掉.
 
+        //这里运行出错,暂时没有找到原因.现行注掉. 找到问题:没有latest_utm_covariance_.resize(POSE_SIZE, POSE_SIZE).
         for (size_t i = 0; i < POSITION_SIZE; i++)
         {
             for (size_t j = 0; j < POSITION_SIZE; j++)
             {
-                std::cout<<"i="<<i<<"\n"<<"j="<<j<<std::endl;
                 latest_utm_covariance_(i, j) = msg->position_covariance[POSITION_SIZE * i + j];
             }
         }
-#endif
 
         if(last_utm_ready == true){
             createOrientation(last_utm,curr_utm);
         }
+
+        gps_update_time_ = msg->header.stamp;
+        gps_updated_ = true;
 
         publishTF();
         publishPoseStamped();
 
         last_utm.utm_x = curr_utm.utm_x;
         last_utm.utm_y = curr_utm.utm_y;
-        last_utm_ready = true;
-        gps_update_time_ = msg->header.stamp;
-        gps_updated_ = true;
+        last_utm_ready = true;        
     }
 }
 
@@ -267,8 +277,9 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "gps2utm_node");
     ros::NodeHandle nh;
 
+    latest_utm_covariance_.resize(POSE_SIZE, POSE_SIZE);
     ros::Subscriber gps_sub = nh.subscribe("gps", 1,gpsFixCallback);
-    pub1_ = nh.advertise<nav_msgs::Odometry>("gnss_pose", 10);
+    pub1_ = nh.advertise<nav_msgs::Odometry>("odom_pose", 10);
     ros::spin();
     return 0;
 }
